04af9c7e481fd48dbc642f3358f528da783dde51
Or <orlevy11@gmail.com>
2022-11-02 12:38:48 +0200

    spot check 1

--- src/App.js
+++ src/App.js
@@ -1,69 +1,64 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   getStuff() {
+    return <h1>Our U_code staff are Lotem and Ameer</h1>;
   }
 
-  getMorningGreeting() {
-  }
+  getMorningGreeting() {}
 
-  getEveningGreeting() {
-  }
+  getEveningGreeting() {}
 
-  showCompany(name, revenue) {
-  }
+  showCompany(name, revenue) {}
 
-  getClassName(temperature) {
-
-  }
+  getClassName(temperature) {}
 
   render() {
-
     let companies = [
       { name: "Tesla", revenue: 140 },
       { name: "Microsoft", revenue: 300 },
-      { name: "Google", revenue: 600 }
-    ]
+      { name: "Google", revenue: 600 },
+    ];
 
     return (
       <div>
         <div className="ex-space">
-          <h4 className='ex-title'>Spot-check 1</h4 >
+          <h4 className="ex-title">Spot-check 1</h4>
           <div className="exercise" id="spotcheck-1">
-            {/* your code here */}
+            {this.getStuff()}
           </div>
         </div>
 
         <div className="ex-space">
-          <h4 className='ex-title'>Spot-check 2</h4>
+          <h4 className="ex-title">Spot-check 2</h4>
           <div className="exercise" id="spotcheck-2">
             {/* your code here */}
           </div>
         </div>
 
         <div className="ex-space">
-          <h4 className='ex-title'>Spot-check 3</h4>
+          <h4 className="ex-title">Spot-check 3</h4>
           <div className="exercise" id="spotcheck-3">
             {/* your code here */}
           </div>
         </div>
 
         <div className="ex-space">
-          <h4 className='ex-title'>Exercise 1</h4>
+          <h4 className="ex-title">Exercise 1</h4>
           <div className="exercise" id="ex-1">
             {/* your code here */}
           </div>
         </div>
 
         <div className="ex-space">
-          <h4 className='ex-title'>Exercise 2</h4>
+          <h4 className="ex-title">Exercise 2</h4>
           <div className="exercise" id="ex-2">
             {/* your code here */}
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
